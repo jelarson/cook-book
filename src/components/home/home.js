ad223e629c3d9c3a5dd3c84474e05a323abe6c1d
jelarson <jesslarson93@gmail.com>
2020-07-15 17:04:26 -0600

    added dropdown boxes for partial, whole, and type of measurements

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,13 +7,40 @@ export default function Home() {
       <div>Hello From Home Header</div>
       <div>
         Hello From Home Body
-        <label htmlFor="cars">Choose a car:</label>
-        <select name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
+        <br />
+        <select name="whole-measurement-num">
+          <option value="blank"> </option>
+          <option value="one">1</option>
+          <option value="two">2</option>
+          <option value="three">3</option>
+          <option value="four">4</option>
+          <option value="five">5</option>
+          <option value="six">6</option>
+          <option value="seven">7</option>
+          <option value="eight">8</option>
+          <option value="nine">9</option>
+          <option value="ten">10</option>
         </select>
+        and
+        <select name="partial-measurement-num">
+          <option value="blank"> </option>
+          <option value="oneEighth">1/8</option>
+          <option value="oneQuarter">1/4</option>
+          <option value="oneThird">1/3</option>
+          <option value="oneHalf">1/2</option>
+          <option value="twoThirds">2/3</option>
+          <option value="threeQuarters">3/4</option>
+        </select>
+        <select name="partial-measurement-num">
+          <option value="blank"> </option>
+          <option value="teaspoons">teaspoons</option>
+          <option value="tablespoons">tablespoons</option>
+          <option value="cups">cups</option>
+          <option value="pints">pints</option>
+          <option value="quarts">quarts</option>
+          <option value="ounces">Ounces</option>
+        </select>
+        of
         <form>
           <input type="text" />
           <input type="text" />
