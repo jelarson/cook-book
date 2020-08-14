08d1ece6d4937589231b15164c63b78c61701e2c
jelarson <jesslarson93@gmail.com>
2020-08-14 11:02:59 -0600

    styled image input box

--- src/components/home/addImageUrlForm.js
+++ src/components/home/addImageUrlForm.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react'
 import { css } from '@emotion/core'
 
+const imageHeaderCss = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+`
+
+const imageInputWrapperCss = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
+
+const imageInputCss = css`
+  border: 2px solid black;
+  width: 80%;
+  border-radius: 7px;
+`
+
 export default function AddImageUrlForm(props) {
   const { actions } = props
   const [imageUrl, setImageUrl] = useState('')
@@ -11,8 +30,10 @@ export default function AddImageUrlForm(props) {
 
   return (
     <div>
-      <div>Recipe Image URL (optional)</div>
-      <input type="text" onChange={(e) => setImageUrl(e.currentTarget.value)} />
+      <div css={imageHeaderCss}>Recipe Image URL (optional):</div>
+      <div css={imageInputWrapperCss}>
+        <input css={imageInputCss} type="text" onChange={(e) => setImageUrl(e.currentTarget.value)} />
+      </div>
       <button type="button" onClick={handleImageButtonClick}>
         Add Image
       </button>
