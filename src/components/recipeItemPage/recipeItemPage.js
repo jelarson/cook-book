f24ee6af479a66b4ea6b4077f2fece7ede6199ed
jelarson <jesslarson93@gmail.com>
2020-08-25 12:27:03 -0600

    skeleton for recipe item page

--- src/components/recipeItemPage/recipeItemPage.js
+++ src/components/recipeItemPage/recipeItemPage.js
@@ -4,6 +4,36 @@ import { css } from '@emotion/core'
 import Navbar from '../navbar/navbar'
 import Footer from '../footer/footer'
 
+const recipeItemPageWrapperCss = css`
+  display: flex;
+  flex-direction: column;
+`
+
+const recipeTitleCss = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  font-size: 1.7em;
+`
+const recipeItemRecipeWrapperCss = css`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`
+
+const recipeImageCss = css`
+  display: flex;
+  width: 40vw;
+  align-items: center;
+  justify-content: center;
+`
+
+const recipeContentWrapperCss = css`
+  display: flex;
+  width: 40vw;
+  border: 2px solid black;
+`
+
 export default function RecipeItemPage(props) {
   const { location } = props
   const { name, category, image, id, ingredients, instructions, thumbsUp, thumbsDown, favorite } = location.state
@@ -11,13 +41,22 @@ export default function RecipeItemPage(props) {
   return (
     <div>
       <Navbar />
+      <div css={recipeItemPageWrapperCss}>
+        <div css={recipeTitleCss}>{name}</div>
+        <div css={recipeItemRecipeWrapperCss}>
+          <div css={recipeImageCss}>
+            Image Placeholder
+            {image}
+          </div>
+          <div css={recipeContentWrapperCss}>
+            {ingredients}
+            {instructions}
+          </div>
+        </div>
+      </div>
       <div>
-        {name}
         {category}
-        {image}
         {id}
-        {ingredients}
-        {instructions}
         {thumbsUp}
         {thumbsDown}
         {favorite}
